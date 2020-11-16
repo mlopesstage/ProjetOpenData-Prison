4cbf250195435e0dc94e9894acb817fb1e6b367f
Lopes Michaël <mica-lopes@hotmail.fr>
2020-11-16 18:43:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ const Command = require('./command/command')
 const Help = require('./command/help')
 const PIB = require('./command/pib')
 const Txc = require('./command/txc')
-const Nbc = require('./command/nbc')
+const Nbi = require('./command/nbi')
 const Nbp = require('./command/nbp')
+const Cog = require('./command/cog')
 const Git = require('./command/git')
 const Sup = require('./command/sup')
 
-//BOT (Photo de profile + Activité
+//Paramètres du bot
 bot.on('ready', function() {
-    //bot.user.setAvatar('./photos/terre.png').catch(console.error)
-    bot.user.setActivity('!help').catch(console.error)
+    bot.user.setAvatar('./photos/terre.png').catch(console.error) //Permets de changer la photo du bot
+    bot.user.setActivity('!help').catch(console.error) //Permets de changer le statut du bot
 })
 
+//Fait les appels nécessaires en fonction de la commande saisie
 bot.on('message', message => {
     Help.parse(message) //Commande !help
     PIB.parse(message) //Commande !pib
     Txc.parse(message) //Commande !txc
-    Nbc.parse(message) //Commande !nbc
+    Nbi.parse(message) //Commande !nbi
     Nbp.parse(message) //Commande !nbp
+    Cog.parse(message) //Commande !cog
     Git.parse(message) //Commande !git
     Sup.parse(message) //Commande !sup
 })
 
+//Token permettant le lancement et la modification du bot sur les serveurs
 bot.login('Nzc1MDEwNDg5NDI3MTY1MjU0.X6gGww.ksL" "imP_rjneYGkWnPS54YlbBNc4')
